1e3705b3fdf1db2000f13d3df30c4bf876732e33
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-05-17 17:50:28 +0530

    add project images

---
+++ src/components/DataBase/projects.js
@@ -0,0 +1,70 @@
+import RKTECHNICALSUPPORT from "../../components/DataBase/ProjectImages/RKTECHNICALSUPPORT.png"
+import DevDetection from "../../components/DataBase/ProjectImages/DevDetection.png"
+import srajinfra from "../../components/DataBase/ProjectImages/srajinfra.png"
+import BharatMarket from "../../components/DataBase/ProjectImages/BharatMarket.png"
+import ComboFinder from "../../components/DataBase/ProjectImages/ComboFinder.png"
+
+const projects = [
+  {
+    id: 1,
+    title: "R K TECHNICAL SUPPORT",
+    description: "Led the development of a professional healthcare clinic website, utilizing HTML, CSS, and JavaScript. Achieved a fully responsive design for mobile, desktop, and tablet, integrating dynamic Google Maps for real-time tracking. Deployed by the client for real-world use on mobile, desktop, and tablet platforms.",
+    image: RKTECHNICALSUPPORT,
+    technologies: ["HTML", "CSS", "JavaScript", "Google Maps API", "Vercel"],
+    github: "https://github.com/RahulChoudhary05/RKTECHNICALSUPPORT",
+    demo: "https://www.rktechs.co/",
+    date: "December 2023 - January 2024",
+  },
+  {
+    id: 1,
+    title: "DevDetective",
+    description: "DevDetective is a sleek and intuitive search engine built with HTML, CSS, and JavaScript, designed to help developers find GitHub repositories quickly and efficiently. With its user-friendly interface, you can easily search for projects based on keywords and topics, making it the perfect tool for discovering valuable code and resources.",
+    image: DevDetection,
+    technologies: ["HTML", "CSS", "JavaScript", "Vercel"],
+    github: "https://github.com/RahulChoudhary05/DevDetection",
+    demo: "https://devdetectionrc05.vercel.app/",
+    date: "May 2024",
+  },
+  {
+    id: 2,
+    title: "BharatMarket",
+    description: "Developed a full-fledged ecommerce website using React, Node.js, Express.js, Tailwind CSS with backend by Firebase. Implemented CI/CD pipeline using Docker and Jenkins for automated testing, building, and deployment, ensuring rapid updates. Implemented features like authentication, real-time database updates, and a smooth checkout process.",
+    image: BharatMarket,
+    technologies: ["React", "Node.js", "Express", "Firebase", "Vercel", "Tailwind CSS", "Docker", "Jenkins", "CI/CD"],
+    github: "https://github.com/RahulChoudhary05/BharatMarket-EcommerceWebsite",
+    demo: "https://blog-platform-demo.com",
+    date: "March 2024 - April 2024",
+  },
+  {
+    id: 3,
+    title: "StudyNotion",
+    description: "Led the development of a fully functional EdTech platform enabling users to create, consume, and rate educational content. Utilized the MERN stack (MongoDB, Express.js, ReactJS, Node.js) to build a seamless learning experience. Created a seamless learning experience and implemented client-server architecture.",
+    image: "https://via.placeholder.com/400x300",
+    technologies: ["React", "Node.js", "Express", "MongoDB", "MERN", "Tailwind CSS", "Vercel"],
+    github: "https://github.com/RahulChoudhary05/StudyNotion",
+    demo: "https://blog-platform-demo.com",
+    date: "June 2024 - August",
+  },
+  {
+    id: 4,
+    title: "S RAJ INFRA PROJECTS PRIVATE LIMITED",
+    description: "Spearheaded the design and development of an enterprise-level SaaS platform, utilizing React and Tailwind CSS. Engineered dynamic user interfaces with Framer Motion animations, achieving 40% improved user engagement metrics. Successfully deployed to production, serving as the primary client acquisition and service showcase platform.",
+    image: srajinfra,
+    technologies: ["React", "Node.js", "Express", "Firebase", "Vercel", "Tailwind CSS"],
+    github: "https://github.com/RahulChoudhary05/S-RAJ-INFRA",
+    demo: "https://srajinfra.vercel.app/",
+    date: "December 2024 - Present",
+  },
+  {
+    id: 5,
+    title: "ComboFinder ",
+    description: "ComboFinder is a web-based tool to upload and process data files efficiently. Users can select specific columns like Product Name or Price and generate refined outputs. Built with React.js, TailwindCSS, and Node.js, it offers a modern UI, secure file handling, and fast data customization for e-commerce, data analysis, and reporting.",
+    image: ComboFinder,
+    technologies: ["React", "Node.js", "Express", "Vercel", "Tailwind CSS"],
+    github: "https://github.com/RahulChoudhary05/ComboFinder",
+    demo: "https://combofinder.vercel.app",
+    date: "March 2025",
+  },
+]
+
+export default projects
